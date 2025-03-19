0a185ab59b23ed0d128b08b9efd9f0216f785a92
Ganesh <ganeshpatelchetti@gmail.com>
2025-03-19 22:07:41 +0530

    updated code

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
+const serverless = require('serverless-http'); 
 
 const authRoutes = require("./routes/authRoutes");
 const quizRoutes = require("./routes/quizRoutes");
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 
 connectDB();
+module.exports.handler = serverless(app); 
 
 app.use("/api", authRoutes);
 app.use("/api", quizRoutes);
